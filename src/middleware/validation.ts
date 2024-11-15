f5413ad325b0815e3ad6313e523f454ed5feeb96
Shiro12222 <leo12190818@gmail.com>
2024-11-15 10:22:22 +0800

    Restaurant Validation Reupdate

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -24,21 +24,9 @@ export const validateMyRestaurantRequest = [
     body("country").isString().notEmpty().withMessage("Country is required"),
     body("deliveryPrice").isNumeric().withMessage("Delivery price must be a number").isFloat({ min: 0 }).withMessage("Delivery price must be greater than zero"),
     body("estimatedDeliveryTime").isNumeric().withMessage("Estimated delivery time must be a number").isInt({ gt: 0 }).withMessage("Estimated delivery time must be greater than zero"),
-    body("cuisines").isArray().withMessage("Cuisines must be an array").notEmpty().withMessage("At least one cuisine is required")
-        .custom(cuisines => {
-            if (cuisines.length === 0) {
-                throw new Error("At least one cuisine is required");
-            }
-            return true;
-        }),     
-    body("menuItems").isArray().withMessage("Menu items must be an array").optional()
-        .custom(menuItems => {
-            if (menuItems.length === 0) {
-                throw new Error("Menu items cannot be empty if provided");
-            }
-            return true;
-        }),
+    body("cuisines").isArray().withMessage("Cuisines must be an array").not().isEmpty().withMessage("cuisine must be an array"),
+    body("menuItems").isArray().withMessage("Menu items must be an array"),
     body("menuItems.*.name").notEmpty().withMessage("MenuItem name is required"),
-    body("menuItems.*.price").isFloat({min: 0}).withMessage("MenuItem price is required and must be positive"),
+    body("menuItems.*.price").notEmpty().isFloat({min: 0}).withMessage("MenuItem price is required and must be positive"),
     handleValidationError,
 ];
